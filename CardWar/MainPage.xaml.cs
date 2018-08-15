c170d3577e0732e8a175881af516a80aaaad67cf
aytona <christopher.aytona@gmail.com>
2018-08-15 12:41:20 -0400

    Updated wireframe

--- CardWar/MainPage.xaml.cs
+++ CardWar/MainPage.xaml.cs
@@ -40,7 +40,7 @@ namespace CardWar
                     Card c1 = deck.DrawCard();
                     Card c2 = deck.DrawCard();
 
-                    TmpMessage.Text += $"C1: {c1.ToString()} (Value - {c1.PointValue()}), C2: {c2.ToString()} (Value - {c2.PointValue()}). C1 Higher Value? {c1.CompareTo(c2)}\n";
+                    //TmpMessage.Text += $"C1: {c1.ToString()} (Value - {c1.PointValue()}), C2: {c2.ToString()} (Value - {c2.PointValue()}). C1 Higher Value? {c1.CompareTo(c2)}\n";
                 } catch (Exception)
                 {
                     emptyDeck = true;
